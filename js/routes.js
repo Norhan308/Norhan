e1c41c8f53d353fad79651fb72c6b12da577f92b
Norhan308 <50364233+Norhan308@users.noreply.github.com>
2020-01-27 00:00:04 +0200

    Add files via upload

---
+++ js/routes.js
@@ -0,0 +1,105 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('mathBagrut', {
+    url: '/login',
+    templateUrl: 'templates/mathBagrut.html',
+    controller: 'mathBagrutCtrl'
+  })
+
+  .state('signup', {
+    url: '/signup',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('page3', {
+    url: '/home',
+    templateUrl: 'templates/page3.html',
+    controller: 'page3Ctrl'
+  })
+
+  .state('page4', {
+    url: '/page4',
+    templateUrl: 'templates/page4.html',
+    controller: 'page4Ctrl'
+  })
+
+  .state('page5', {
+    url: '/page5',
+    templateUrl: 'templates/page5.html',
+    controller: 'page5Ctrl'
+  })
+
+  .state('page6', {
+    url: '/page6',
+    templateUrl: 'templates/page6.html',
+    controller: 'page6Ctrl'
+  })
+
+  .state('page9', {
+    url: '/page9',
+    templateUrl: 'templates/page9.html',
+    controller: 'page9Ctrl'
+  })
+
+  .state('page12', {
+    url: '/page12',
+    templateUrl: 'templates/page12.html',
+    controller: 'page12Ctrl'
+  })
+
+  .state('page15', {
+    url: '/page15',
+    templateUrl: 'templates/page15.html',
+    controller: 'page15Ctrl'
+  })
+
+  .state('page7', {
+    url: '/page7',
+    templateUrl: 'templates/page7.html',
+    controller: 'page7Ctrl'
+  })
+
+  .state('page8', {
+    url: '/page8',
+    templateUrl: 'templates/page8.html',
+    controller: 'page8Ctrl'
+  })
+
+  .state('801', {
+    url: '/page10',
+    templateUrl: 'templates/801.html',
+    controller: '801Ctrl'
+  })
+
+  .state('page11', {
+    url: '/page11',
+    templateUrl: 'templates/page11.html',
+    controller: 'page11Ctrl'
+  })
+
+  .state('2019801', {
+    url: '/page13',
+    templateUrl: 'templates/2019801.html',
+    controller: '2019801Ctrl'
+  })
+
+  .state('page14', {
+    url: '/page14',
+    templateUrl: 'templates/page14.html',
+    controller: 'page14Ctrl'
+  })
+
+$urlRouterProvider.otherwise('/login')
+
+
+});
